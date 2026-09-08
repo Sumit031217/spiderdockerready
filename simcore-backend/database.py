9503c88b2d1fa395147705e41555ad7d91982eb7
debu4044 <devshel34@gmail.com>
2026-09-09 00:05:28 +0530

    30 day worthy run

--- simcore-backend/database.py
+++ simcore-backend/database.py
@@ -8,7 +8,11 @@ SQLALCHEMY_DATABASE_URL = os.getenv(
     "DATABASE_URL", 
     "postgresql://postgres:702073@localhost:5432/simcore_db5"
 )
-engine = create_engine(SQLALCHEMY_DATABASE_URL)
+engine = create_engine(
+    SQLALCHEMY_DATABASE_URL, 
+    pool_pre_ping=True, 
+    pool_recycle=1800
+)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
 
